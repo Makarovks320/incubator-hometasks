670b53e94931a5dd0f26e2653c8a49fc0eeb220e
Kirill Makarov <kirill.makarov@visitech.ru>
2023-03-19 10:30:38 +0300

    json parser

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import express, {Request, Response} from 'express';
 const PORT = process.env.PORT || 3000;
 export const app = express();
 
+const jsonParser = express.json();
+app.use(jsonParser);
+
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
 app.listen(PORT, () => {
-    console.log('Server is running at port ' + PORT);
+    console.log(`Server is running at http://localhost:${PORT}/`);
 });
