cf649c7bf794b4085b386e0d8f9012ad9bddf782
Kirill Makarov <kirill.makarov@visitech.ru>
2023-05-27 18:48:00 +0300

    добавил 2 эндпоинта в блог-роутер

--- src/Routers/blogsRouter.ts
+++ src/Routers/blogsRouter.ts
@@ -1,9 +1,15 @@
 import {Request, Response, Router} from "express";
+import {param} from "express-validator";
 import {blogService} from "../domain/blogService";
 import {authorization} from "../Middlewares/authorization";
 import {descriptionValidation, nameValidation, websiteUrlValidation} from "../Middlewares/blogsValidations";
 import {inputValidator} from "../Middlewares/inputValidator";
 import {BlogQueryParams, blogsQueryRepository} from "../Repositories/blogsQueryRepository";
+import {PostQueryParams, postsQueryRepository} from "../Repositories/postsQueryRepository";
+import {checkBlogIdExists} from "../Middlewares/checkBlogIdExists";
+import {blogExistingValidator} from "../Middlewares/blogExistingValidator";
+import {contentValidation, shortDescriptionValidation, titleValidation} from "../Middlewares/postsValidations";
+import {InputPost, postService} from "../domain/postService";
 
 export const blogsRouter = Router();
 blogsRouter.get('/', async (req: Request, res: Response) => {
@@ -12,7 +18,7 @@ blogsRouter.get('/', async (req: Request, res: Response) => {
         pageNumber: parseInt(req.query.pageNumber as string) || 1,
         pageSize: parseInt(req.query.pageSize as string) || 10,
         sortBy: String(req.query.sortBy) || 'createdAt',
-        sortDirection: req.query.sortDirection === 'asc' ?'asc' : 'desc'
+        sortDirection: req.query.sortDirection === 'asc' ? 'asc' : 'desc'
     }
     const blogs = await blogsQueryRepository.getBlogs(queryParams);
     res.send(blogs);
@@ -23,7 +29,7 @@ blogsRouter.get('/:id', async (req: Request, res: Response) => {
     res.send(blogs);
 });
 
-blogsRouter.post('/',
+blogsRouter.post('/', [
     authorization,
     nameValidation,
     websiteUrlValidation,
@@ -32,9 +38,45 @@ blogsRouter.post('/',
     async (req: Request, res: Response) => {
         const blogs = await blogService.createNewBlog(req.body);
         res.send(blogs);
-    });
+    }
+]);
 
-blogsRouter.put('/:id',
+blogsRouter.get('/:id/posts', async (req: Request, res: Response) => {
+    const blog = await blogService.getBlogById(req.params.id);
+    if (blog) {
+        const queryParams: PostQueryParams = {
+            pageNumber: parseInt(req.query.pageNumber as string) || 1,
+            pageSize: parseInt(req.query.pageSize as string) || 10,
+            sortBy: String(req.query.sortBy) || 'createdAt',
+            sortDirection: req.query.sortDirection === 'asc' ?'asc' : 'desc'
+        }
+        const posts = await postsQueryRepository.getPosts(queryParams, req.params.id);
+        res.send(posts);
+    } else {
+        res.status(404).send();
+    }
+});
+
+blogsRouter.post('/:id/posts', [
+    authorization,
+    param('id').custom(checkBlogIdExists).withMessage('blog is not found'),
+    blogExistingValidator,
+    titleValidation,
+    shortDescriptionValidation,
+    contentValidation,
+    inputValidator,
+    async (req: Request, res: Response) => {
+        const post: InputPost = {
+            ...req.body,
+            blogId: req.params.id,
+            blogName: req.body.blogName
+        }
+        const newPost = await postService.createNewPost(post);
+        res.status(201).send(newPost);
+    }
+]);
+
+blogsRouter.put('/:id', [
     authorization,
     nameValidation,
     websiteUrlValidation,
@@ -43,15 +85,16 @@ blogsRouter.put('/:id',
     async (req: Request, res: Response) => {
         const newBlog = await blogService.updateBlogById(req.params.id, req.body);
         newBlog ? res.send(newBlog) : res.send(404);
+    }
+]);
 
-    });
-
-blogsRouter.delete('/',
+blogsRouter.delete('/', [
     authorization,
     async (req: Request, res: Response) => {
         await blogService.deleteAllBlogs();
         res.sendStatus(204);
-    });
+    }
+]);
 
 blogsRouter.delete('/:id', [
     authorization,
