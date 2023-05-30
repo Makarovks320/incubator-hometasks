6ac2c46436b0e9ea29bc9c8b222f0562b6239b5a
Kirill Makarov <kirill.makarov@visitech.ru>
2023-05-31 00:54:12 +0300

    testing/all-data делает реквесты на другие роуты

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express, {Request, Response} from 'express';
 import {runDb} from "./Repositories/db";
 import {postsRouter} from "./Routers/postsRouter";
 import {blogsRouter} from "./Routers/blogsRouter";
+import axios from "axios";
 
 const PORT = process.env.PORT || 3000;
 export const app = express();
@@ -12,6 +13,29 @@ app.use(jsonParser);
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
+app.delete('/testing/all-data', async (req: Request, res: Response) => {
+    const basicAuth = req.headers.authorization; // Получаем заголовок авторизации из исходного запроса
+
+    try {
+        const deleteBlogs = axios.delete(`http://localhost:${PORT}/blogs`, {
+            headers: {authorization: basicAuth} // Передаем заголовок авторизации в запрос на /blogs
+        });
+
+        const deletePosts = axios.delete(`http://localhost:${PORT}/posts`, {
+            headers: {authorization: basicAuth} // Передаем заголовок авторизации в запрос на /posts
+        });
+
+        const [blogsResponse, postsResponse] = await Promise.all([deleteBlogs, deletePosts]);
+
+        if (blogsResponse.status === 204 && postsResponse.status === 204) {
+            res.status(204);
+        }
+        else res.status(500).send('internal server error');
+    } catch (error) {
+        // @ts-ignore
+        res.status(500).send(error.message);
+    }
+})
 
 app.use('/posts', postsRouter);
 app.use('/blogs', blogsRouter);
@@ -22,4 +46,5 @@ async function startApp() {
         console.log(`Server is running at http://localhost:${PORT}/`);
     });
 }
+
 startApp();
