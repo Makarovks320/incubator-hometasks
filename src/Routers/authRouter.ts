0812b9019a755f679e2e83c5851d66813e7dc8af
Kirill Makarov <kirill.makarov@visitech.ru>
2023-06-23 23:46:23 +0300

    fix: auth/login, feat: authValidation

--- src/Routers/authRouter.ts
+++ src/Routers/authRouter.ts
@@ -1,17 +1,23 @@
 import {Request, Response, Router} from "express";
 import {userService} from "../domain/userService";
 import {jwtService} from "../application/jwtService";
+import { loginOrEmailAuthValidation, passwordAuthValidation } from "../Middlewares/authValidations";
+import { inputValidator } from "../Middlewares/inputValidator";
 
 export const authRouter = Router();
-authRouter.post('/login', async (req: Request, res: Response) => {
+authRouter.post('/login', [
+    loginOrEmailAuthValidation,
+    passwordAuthValidation,
+    inputValidator,
+    async (req: Request, res: Response) => {
     const user = await userService.checkCredentials(req.body.loginOrEmail, req.body.password);
     if (user) {
         const token = await jwtService.createToken(user);
-        res.status(201).send(token);
+        res.status(200).send({accessToken: token});
     } else {
         res.sendStatus(401);
     }
-});
+}]);
 authRouter.get('/me', async (req: Request, res: Response) => {
     const user = await userService.checkCredentials(req.body.loginOrEmail, req.body.password);
     if (user) {
