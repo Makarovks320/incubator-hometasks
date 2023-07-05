3d1a63c3784faf3794b8cb10bea6e65523c13aec
Kirill Makarov <kirill.makarov@visitech.ru>
2023-07-05 23:24:54 +0300

    fix: 403 for PUT

--- src/Routers/commentsRouter.ts
+++ src/Routers/commentsRouter.ts
@@ -1,28 +1,30 @@
 import {Request, Response, Router} from "express";
-import {commentService, InputComment, InputCommentWithPostId} from "../domain/commentService";
+import {commentService, InputComment} from "../domain/commentService";
 import {CommentOutput} from "../Repositories/commentsRepository";
 import {authMiddleware} from "../Middlewares/authMiddleware";
 import {OutputUser, userService} from "../domain/userService";
-import {param} from "express-validator";
-import {checkPostExists} from "../Middlewares/checkPostExists";
-import {idFromUrlExistingValidator} from "../Middlewares/idFromUrlExistingValidator";
 import {commentContentValidation} from "../Middlewares/commentValidations";
 import {inputValidator} from "../Middlewares/inputValidator";
-import {checkCommentExists} from "../Middlewares/checkCommentExists";
 
 export const commentsRouter = Router();
 
 commentsRouter.put('/:id', [
     authMiddleware,
-    param('id').custom(checkCommentExists).withMessage('post is not found'),
-    idFromUrlExistingValidator,
     commentContentValidation,
     inputValidator,
     async (req: Request, res: Response) => {
-        const comment: InputComment = {
+        const oldComment: CommentOutput | null = await commentService.getCommentById(req.params.id);
+        if (!oldComment) {
+            res.status(404).send('Comment is not found');
+        }
+        const user = await userService.findUserById(req.userId!) as OutputUser;
+        if (oldComment!.commentatorInfo.userLogin != user.login) {
+            res.status(403).send('Comment is not your own');
+        }
+        const commentForUpdate : InputComment = {
             content: req.body.content,
         }
-        const isUpdated = await commentService.updateComment(comment, req.params.id);
+        const isUpdated = await commentService.updateComment(commentForUpdate, req.params.id);
         isUpdated? res.send(204) : res.send(404);
     }
 ]);
