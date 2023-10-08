2cff7677d76495bdeff1f4673ce77d6e0b180138
Kirill Makarov <kirill.makarov@visitech.ru>
2023-10-09 02:47:07 +0300

    fix: '/testing/all-data' delete all in promise

--- src/index.ts
+++ src/index.ts
@@ -21,46 +21,20 @@ app.use(cookieParser());
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World !!');
 });
-app.delete('/testing/all-data',
-    // async (req: Request, res: Response) => {
-    // const basicAuth = req.headers.authorization; // Получаем заголовок авторизации из исходного запроса
-    //
-    // try {
-    //     const deleteBlogs = axios.delete(`http://localhost:${PORT}/blogs`, {
-    //         headers: {authorization: basicAuth} // Передаем заголовок авторизации в запрос на /blogs
-    //     });
-    //
-    //     const deletePosts = axios.delete(`http://localhost:${PORT}/posts`, {
-    //         headers: {authorization: basicAuth} // Передаем заголовок авторизации в запрос на /posts
-    //     });
-    //
-    //     const [blogsResponse, postsResponse] = await Promise.all([deleteBlogs, deletePosts]);
-    //
-    //     if (blogsResponse.status === 204 && postsResponse.status === 204) {
-    //         res.status(204);
-    //     }
-    //     else res.status(500).send('internal server error');
-    // } catch (error) {
-    //     // @ts-ignore
-    //     res.status(500).send(error.message);
-    // }
-// }
-    async (req: Request, res: Response, next: NextFunction) => {
-        await postService.deleteAllPosts();
-        next();
-    },
-    async (req: Request, res: Response, next: NextFunction) => {
-        await blogService.deleteAllBlogs();
-        next();
-    },
-    async (req: Request, res: Response, next: NextFunction) => {
-        await userService.deleteAllUsers();
-        next();
-    },
-    async (req: Request, res: Response) => {
-        await commentService.deleteAllComments();
-        res.sendStatus(204);
-    },
+
+app.delete('/testing/all-data', async (req: Request, res: Response, next: NextFunction) => {
+        Promise.all([
+            postService.deleteAllPosts(),
+            blogService.deleteAllBlogs(),
+            userService.deleteAllUsers(),
+            commentService.deleteAllComments()
+        ]).catch((e) => {
+            console.log(e.message);
+            return res.sendStatus(500);
+        })
+
+        return res.sendStatus(204)
+    }
 )
 
 app.use('/posts', postsRouter);
