5cbf7dba3fb9f91873efa7d46391b16692c4e9e3
Kirill Makarov <kirill.makarov@visitech.ru>
2023-10-10 03:13:04 +0300

    refactor: добавил контроллер '/users'

---
+++ src/Controller/users-controller.ts
@@ -0,0 +1,64 @@
+import {Request, Response} from "express";
+import {InputUser, userService} from "../domain/user-service";
+import {STATUSES_HTTP} from "../enums/http-statuses";
+import {UserQueryParams, usersQueryRepository} from "../Repositories/users-query-repository";
+import mongoose from "mongoose";
+import {ObjectId} from "mongodb";
+
+type UserForResponseType = {
+    id: string,
+    login: string,
+    email: string,
+    createdAt: string
+}
+
+export const usersController = {
+
+    async createNewUser(req: Request, res: Response) {
+        const newUserInput: InputUser = {
+            login: req.body.login,
+            email: req.body.email,
+            password: req.body.password
+        }
+        const createdUser = await userService.createUser(newUserInput);
+        const userForResponse: UserForResponseType = {
+            id: createdUser._id.toString(),
+            login: createdUser.accountData.userName,
+            email: createdUser.accountData.email,
+            createdAt: createdUser.accountData.createdAt.toString()
+        }
+        res.status(STATUSES_HTTP.CREATED_201).send(userForResponse);
+    },
+
+    async getUsers(req: Request, res: Response) {
+        const queryParams: UserQueryParams = {
+            searchLoginTerm: req.query.searchLoginTerm as string || null,
+            searchEmailTerm: req.query.searchEmailTerm as string || null,
+            pageNumber: parseInt(req.query.pageNumber as string) || 1,
+            pageSize: parseInt(req.query.pageSize as string) || 10,
+            sortBy: req.query.sortBy?.toString() || 'createdAt',
+            sortDirection: req.query.sortDirection === 'asc' ? 'asc' : 'desc'
+        }
+        const users = await usersQueryRepository.getUsers(queryParams);
+        res.send(users);
+    },
+
+    async getUserById(req: Request, res: Response) {
+        const stringId = req.params.id;
+        const objectId = new mongoose.Types.ObjectId(stringId);
+        const user = await userService.findUserById(objectId as ObjectId);
+        user ? res.send(user) : res.send(STATUSES_HTTP.NOT_FOUND_404);
+    },
+
+    async deleteUserById(req: Request, res: Response) {
+        const stringId = req.params.id;
+        const objectId = new mongoose.Types.ObjectId(stringId);
+        const user = await userService.deleteUserById(objectId);
+        user ? res.status(STATUSES_HTTP.NO_CONTENT_204).send() : res.status(STATUSES_HTTP.NOT_FOUND_404).send();
+    },
+
+    async deleteAllUsers(req: Request, res: Response) {
+        await userService.deleteAllUsers();
+        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204);
+    }
+}
--- src/Routers/users-router.ts
+++ src/Routers/users-router.ts
@@ -1,20 +1,9 @@
-import {Request, Response, Router} from "express";
-import {InputUser, userService} from "../domain/user-service";
-import {UserQueryParams, usersQueryRepository} from "../Repositories/users-query-repository";
+import {Router} from "express";
 import {authorization} from "../Middlewares/authorization";
 import {emailValidation, loginValidation, passwordValidation} from "../Middlewares/users-validations";
 import {inputValidator} from "../Middlewares/input-validator";
-import {ObjectId} from "mongodb";
-import * as mongoose from "mongoose";
-import {STATUSES_HTTP} from "../enums/http-statuses";
+import {usersController} from "../Controller/users-controller";
 
-
-type UserForResponseType = {
-    id: string,
-    login: string,
-    email: string,
-    createdAt: string
-}
 
 export const usersRouter = Router();
 usersRouter.post('/', [
@@ -23,63 +12,25 @@ usersRouter.post('/', [
     passwordValidation,
     emailValidation,
     inputValidator,
-    async (req: Request, res: Response) => {
-        const newUserInput: InputUser = {
-            login: req.body.login,
-            email: req.body.email,
-            password: req.body.password
-        }
-        const createdUser = await userService.createUser(newUserInput);
-        const userForResponse: UserForResponseType = {
-            id: createdUser._id.toString(),
-            login: createdUser.accountData.userName,
-            email: createdUser.accountData.email,
-            createdAt: createdUser.accountData.createdAt.toString()
-        }
-        res.status(STATUSES_HTTP.CREATED_201).send(userForResponse);
-    }
+    usersController.createNewUser
 ]);
 
 usersRouter.get('/', [
     authorization,
-    async (req: Request, res: Response) => {
-        const queryParams: UserQueryParams = {
-            searchLoginTerm: req.query.searchLoginTerm as string || null,
-            searchEmailTerm: req.query.searchEmailTerm as string || null,
-            pageNumber: parseInt(req.query.pageNumber as string) || 1,
-            pageSize: parseInt(req.query.pageSize as string) || 10,
-            sortBy: req.query.sortBy?.toString() || 'createdAt',
-            sortDirection: req.query.sortDirection === 'asc' ? 'asc' : 'desc'
-        }
-        const users = await usersQueryRepository.getUsers(queryParams);
-        res.send(users);
-    }
+    usersController.getUsers
 ]);
 
 usersRouter.get('/:id', [
     authorization,
-    async (req: Request, res: Response) => {
-        const stringId = req.params.id;
-        const objectId = new mongoose.Types.ObjectId(stringId);
-        const user = await userService.findUserById(objectId as ObjectId);
-        user ? res.send(user) : res.send(STATUSES_HTTP.NOT_FOUND_404);
-    }
+    usersController.getUserById
 ]);
 
 usersRouter.delete('/:id', [
     authorization,
-    async (req: Request, res: Response) => {
-        const stringId = req.params.id;
-        const objectId = new mongoose.Types.ObjectId(stringId);
-        const user = await userService.deleteUserById(objectId);
-        user ? res.status(STATUSES_HTTP.NO_CONTENT_204).send() : res.status(STATUSES_HTTP.NOT_FOUND_404).send();
-    }
+    usersController.deleteUserById
 ]);
 
 usersRouter.delete('/', [
-    async (req: Request, res: Response) => {
-        await userService.deleteAllUsers();
-        res.sendStatus(STATUSES_HTTP.NO_CONTENT_204);
-    }
+    usersController.deleteAllUsers
 ]);
 
