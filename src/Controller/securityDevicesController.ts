d2596e4d7cf756221e9904254822987aeae26075
Kirill Makarov <kirill.makarov@visitech.ru>
2023-10-20 22:49:29 +0300

    fix

--- src/Controller/securityDevicesController.ts
+++ src/Controller/securityDevicesController.ts
@@ -1,5 +1,4 @@
 import {Request, Response} from "express";
-import {sessionsRepository} from "../Repositories/sessions-repository";
 import {jwtService, RefreshTokenInfoType} from "../application/jwt-service";
 import {STATUSES_HTTP} from "../enums/http-statuses";
 import {sessionService} from "../domain/session-service";
@@ -7,10 +6,20 @@ import {sessionService} from "../domain/session-service";
 export const securityDevicesController = {
     async getAllSessionsForUser(req: Request, res: Response) {
         const refreshToken = req.cookies?.refreshToken;
-        if (!refreshToken) res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
+        if (!refreshToken) {
+            res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
+        }
         const refreshTokenInfo: RefreshTokenInfoType | null = await jwtService.getRefreshTokenInfo(refreshToken);
-        if (refreshTokenInfo.userId) res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
+        if (!refreshTokenInfo?.userId) {
+            res.sendStatus(STATUSES_HTTP.UNAUTHORIZED_401);
+        }
         const sessions = await sessionService.getAllSessionsForUser(refreshTokenInfo.userId);
         res.status(STATUSES_HTTP.OK_200).send(sessions);
+    },
+
+    async deleteSessionByDeviceId(req: Request, res: Response) {
+        // should return error if :id from uri param not found; status 404;
+        const deviceId = req.params.deviceId;
+
     }
 }
