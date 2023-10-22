2bd062c69d870b1846aafb366548e41c699eac44
Kirill Makarov <kirill.makarov@visitech.ru>
2023-10-23 00:03:44 +0300

    Для вопроса менторам

--- src/Application/jwt-service.ts
+++ src/Application/jwt-service.ts
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
 import {settings} from "../settings";
 import {ObjectId} from "mongodb";
+import mongoose from "mongoose";
 
 const secret: string = settings.JWT_SECRET;
 const refreshSecret: string = settings.JWT_REFRESH_SECRET;
@@ -29,12 +30,13 @@ export const jwtService = {
         // здесь надо обновить сессию
         return jwt.sign({userId}, refreshSecret, {expiresIn: 20});
     },
-    async getUserIdByToken(token: string): Promise<string | null> {
+    async getUserIdByToken(token: string): Promise<ObjectId | null> {
         try {
-            // делаем в цикле try-catch, т.к. verify() может вернуть ошибку
             const result: any = await jwt.verify(token, secret);
-            // todo: почему verify возвращает строку? хотя получал ObjectId
-            return result.userId
+            // todo: обсудить, почему создаем userId как ObjectId, а возвращается string.
+            // todo: почему verify возвращает result.userId как строку? хотя получал ObjectId
+            const userId = new mongoose.Types.ObjectId(result.userId);
+            return userId;
         } catch (e) {
             return null;
         }
@@ -42,12 +44,13 @@ export const jwtService = {
     getRefreshTokenInfo(refreshToken: string): RefreshTokenInfoType | null {
         try {
             const result: any = jwt.verify(refreshToken, secret)
-            debugger;
             return {
                 deviceId: result.deviceId,
                 iat: result.iat * 1000,
                 exp: result.exp * 1000,
-                userId: result.userId
+                // todo: обсудить, почему создаем userId как ObjectId, а возвращается string.
+                // todo: почему verify возвращает id как строку? хотя получал ObjectId
+                userId: new mongoose.Types.ObjectId(result.userId)
             }
         } catch (e) {
             return null
