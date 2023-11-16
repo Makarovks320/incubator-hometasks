de2495792fd32a5b802660b35d64a691ac34ec35
Kirill Makarov <kirill.makarov@visitech.ru>
2023-11-16 18:49:23 +0300

    fix: delete unused Mongo DB legacy

--- __tests__/utils/test_utilities.ts
+++ __tests__/utils/test_utilities.ts
@@ -9,10 +9,6 @@ dotenv.config();
 const mongoUri: string = process.env.MONGO_CLOUD_URL || '';
 if (!mongoUri) throw new Error('db uri is not passed');
 
-const DbName =  process.env.MONGO_DB_NAME || "incubator-project";
-
-export const connection_string = `${mongoUri}/${DbName}`;
-
 export const authBasicHeader = {Authorization: 'Basic YWRtaW46cXdlcnR5'};
 
 export function generateString(length: number): string {
