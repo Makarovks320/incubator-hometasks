ee2710b0ce13d035cac233275d9e6fa6d92e0383
Kirill Makarov <kirill.makarov@visitech.ru>
2023-12-22 20:08:48 +0300

    test: refactor + add spy

--- src/services/auth-service.test.ts
+++ src/services/auth-service.test.ts
@@ -9,6 +9,7 @@ import mongoose from "mongoose";
 import {UserModel} from "../models/user/user-db-model";
 import {ObjectId} from "mongodb";
 import {addMinutes} from "date-fns";
+import {randomUUID} from "crypto";
 
 //jest.setTimeout(100000000);// для дебаггинга
 
@@ -36,87 +37,82 @@ describe('integration test for AuthService', () => {
     const authService = new AuthService(usersRepository, jwtService, emailManager);
 
     describe('create user', () => {
+        const email1 = 'email1@mail.com';
+        const login1 = 'login1';
+        const email2 = 'email2@mail.com';
+        const login2 = 'login2';
 
         it('email Adapter Mock should be called', async () => {
-            const email = 'email1@mail.com';
-            const login = 'login1';
-            const result = await authService.createUser(login, email, 'password123');
+            const result = await authService.createUser(login1, email1, 'password123');
             if (!result) throw new Error('test can not be performed');
 
             expect(emailAdapterMock.sendEmail).toBeCalled();
         });
 
         it('should return correct created user', async () => {
-            const email = 'email2@mail.com';
-            const login = 'login2';
-            const result = await authService.createUser(login, email, 'password123');
+            const result = await authService.createUser(login2, email2, 'password123');
             if (!result) throw new Error('test can not be performed');
-            expect(result.accountData.email).toBe(email);
-            expect(result.accountData.userName).toBe(login);
+            expect(result.accountData.email).toBe(email2);
+            expect(result.accountData.userName).toBe(login2);
             expect(result.emailConfirmation.isConfirmed).toBe(false);
         })
     });
 
     describe('confirm email', () => {
+        const emailUser1 = 'email1@mail.com';
+        const loginUser1 = 'login1';
+        const confirmationCode1 = 'code1';
+        const emailUser2 = 'email2@mail.com';
+        const loginUser2 = 'login2';
+        const confirmationCode2 = 'code2';
+
+        function createUserToInsertToDB (loginUser: string, emailUser: string, confirmationCode: string, expirationDate: Date) {
+            return {
+                _id: new ObjectId(),
+                accountData: {
+                    userName: loginUser,
+                    email: emailUser,
+                    salt: randomUUID(),
+                    hash: randomUUID(),
+                    createdAt: new Date(),
+                },
+                emailConfirmation: {
+                    confirmationCode: confirmationCode,
+                    isConfirmed: false,
+                    expirationDate: expirationDate,
+                },
+                passwordRecovery: {
+                    passwordRecoveryCode: "",
+                    active: false
+                }
+            }
+        }
+
         it('should return false for expired confirmation code', async () => {
             await UserModel.insertMany([
-                {
-                    _id: new ObjectId(),
-                    accountData: {
-                        userName: "name 1",
-                        email: "email@email.email",
-                        salt: "salt",
-                        hash: "hash",
-                        createdAt: new Date(),
-                    },
-                    emailConfirmation: {
-                        confirmationCode: "some confirmation code",
-                        isConfirmed: false,
-                        expirationDate: addMinutes(new Date, -1),
-                    },
-                    passwordRecovery: {
-                        passwordRecoveryCode: "",
-                        active: false
-                    }
-                }
+                createUserToInsertToDB(loginUser1, emailUser1, confirmationCode1, addMinutes(new Date, -1))
             ]);
 
-            const result = await authService.confirmEmailByCodeOrEmail("some confirmation code");
+            const result = await authService.confirmEmailByCodeOrEmail(confirmationCode1);
 
             expect(result).toBeFalsy();
         })
 
         it('should return false for not existing confirmation code', async () => {
-
-            const result = await authService.confirmEmailByCodeOrEmail("WRONG confirmation code");
+            // начинаем следить за функцией с помощью обертки-шпиона:
+            const spy = jest.spyOn(usersRepository, 'confirmUserById');
+            const result = await authService.confirmEmailByCodeOrEmail(confirmationCode1 + "WRONG");
 
             expect(result).toBeFalsy();
+            expect(spy).not.toBeCalled();
         })
 
         it('should return true for existing and not expired confirmation code', async () => {
             await UserModel.insertMany([
-                {
-                    _id: new ObjectId(),
-                    accountData: {
-                        userName: "name 2",
-                        email: "email2@email.email",
-                        salt: "salt",
-                        hash: "hash",
-                        createdAt: new Date(),
-                    },
-                    emailConfirmation: {
-                        confirmationCode: "good confirmation code",
-                        isConfirmed: false,
-                        expirationDate: addMinutes(new Date, 5),
-                    },
-                    passwordRecovery: {
-                        passwordRecoveryCode: "",
-                        active: false
-                    }
-                }
+                createUserToInsertToDB(loginUser2, emailUser2, confirmationCode2, addMinutes(new Date, 1))
             ]);
 
-            const result = await authService.confirmEmailByCodeOrEmail("good confirmation code");
+            const result = await authService.confirmEmailByCodeOrEmail(confirmationCode2);
 
             expect(result).toBeTruthy();
         })
